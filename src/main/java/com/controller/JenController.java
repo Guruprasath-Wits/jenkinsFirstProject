8559cc595bdd1dcd1fd6bb3f23dc71693411a131
guruprasathonesoft <guruprasathonesoft@gmail.com>
2025-11-06 14:59:56 +0530

    nov6

--- src/main/java/com/controller/JenController.java
+++ src/main/java/com/controller/JenController.java
@@ -10,6 +10,6 @@ public class JenController {
 
 	@GetMapping(value="/getTest")
 	 public String getTest() {
-		  return "Hi Welcome to jenkins Auto CI and CD ";	
+		  return "Hi Welcome to jenkins Auto CI and CD after git add";	
 }
 }
